15d01f2ad6c443b62f25758695e203f333e4101c
Kaue Christian <k17christian@gmail.com>
2023-08-16 00:08:03 -0300

    upando exemplo

---
+++ exemplo.py
@@ -0,0 +1,10 @@
+frase = str(input('Digite uma senha: '))
+lista = []
+lista = frase
+
+if lista == 'Aa':
+    print('001')
+elif lista == 'Bb':
+    print('010')
+else:
+    print('Não é possivel criptografar. ')
