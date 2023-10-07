d460be1ca7c17cfabb0aaf6b09675e60aef74f9d
yanderline1 <yanderline1@gmail.com>
2023-10-07 14:24:13 -0300

    feitas as funções de taylor e macLaunrin como chave publica e privado da criptografia resta a implementação e teste das funções

--- Testando.py
+++ Testando.py
@@ -1,22 +1,15 @@
-import CalculoOtimizado as CO
 import yanderer
-
+import math
+import sympy as sp
 
 # teste 1
-# num_test = random.randint(0,1221)
-# print(num_test)
-# print(yanderer.teste_de_func(num_test))
-
-# teste 2
-# teste = "MinhaSenha15689"
-# print(yanderer.encrip(teste, 2))
-# nesta segunda função, para cada caractere individual presente na lista, lê-se o resultado da direita para a esquerda.
-
-# teste 3
-bases, valores = yanderer.convert(12345678)
+# bases, valores = yanderer.convert(12345678)
 # print(bases)
 # print(valores)
-print(yanderer.deencrip(valores, bases))
+# print(yanderer.deencrip(valores, bases))
 
+x = sp.symbols('x')
+funcao = (x+2)**2
 
-
+resultado = yanderer.calculus(funcao)
+print(resultado)
--- yanderer.py
+++ yanderer.py
@@ -2,11 +2,6 @@ import random
 import math
 import sympy as sp
 
-# Instruções: a função encrip já funciona para a conversão de números usando bases de números com ponto flutuante, resta programar
-# a função para gerar a base de conversão usando a função de limite cuja a função será F(y) = (x + 3 ** (1/2))**2.
-# (Z é igual ao limite de F(y) quando X tende a O) onde Z é a  base que será usada para conversão, y é a posição do algarismo que será convertido,
-# X é o algarismo que será convertido e O é o número máximo da base que estará sendo utilizada.
-
 def convert(corda):
     # só transforma a string recebida nos seus respectivos valores em ascii
     chars = map(ord, list(str(corda)))
@@ -15,13 +10,17 @@ def convert(corda):
     resultado = []
     indice = []
     
+    # Definição da função limite
+    # X = sp.Symbol('X')
+    # funcao = (X + 2)**2
+    
     # Gera numeros aleatórios que serão as bases de conversão para cada caracter presente na string
     for i in chars:
         base = random.uniform(2.0, 10.0) # 10 > base >= 2
         baseval.append(base)
-        valor = []
-    
+
     # Realiza a conversão dos caracteres para as bases geradas usando o método da divisão
+        valor = []
         while(i != 0):
             valor.append(str(i % base))
             i = i // base
@@ -33,19 +32,36 @@ def convert(corda):
         resultado.append(val)
 
     return baseval, resultado # Retorna uma tupla com duas listas, uma contem as bases usadas para conversão em cada caractere
-                              # a outra contem uma as listas com os números individuais para cada caractere
+                              # a outra contem as listas com os números individuais para cada algarismo dos numeros convertidos
 
 # função de desencriptação
 def deencrip(entrada, bases):
     corda = []
     # acessa cada lista (valor de caracter) presente na lista entrada
     for num,char in enumerate(entrada):
-        calcu = 0
+        calc = 0
     
     # acessa cada número individual da lista filha para realizar a desconversão usando a base correspondente
         for posix,valor in enumerate(char[::-1]):
-            calcu += float(valor)*(bases[num]**posix)
-        corda.append(calcu)
+            calc += float(valor)*(bases[num]**posix)
+        corda.append(chr(int(calc)))
     
     return corda #retorna uma lista com os valores ascii correspondentes aos caracteres da string inserida inicialmente em formato float
     
+#função pra calcular o polinomio de taylor, recebe como argumentos a função matemática na qual se deseja calcular o polinomio
+#e Cvalue que representa o ponto no plano cartesiano em torno do qual se da o cálculo
+#nem tenta entender o que ta escrito pq nem eu entendi direito ksksksks o importante e que essa função atuará como chave privada
+def taylors(func, Cvalue):
+    result = 0
+    for n in range(0, 10):
+        calculo = ((sp.diff(func.subs(sp.symbols('x'), Cvalue), sp.symbols('x'), n)) / math.factorial(n)) * (sp.symbols('x') - Cvalue)**n
+        result += calculo
+    return result
+
+
+def calculus (func):
+    result = 0
+    for n in range(0, 10):
+        calculo = ((sp.diff(func.subs(sp.symbols('x'), 0), sp.symbols('x'), n)) / math.factorial(n)) * (sp.symbols('x'))**n
+        result += calculo
+    return result
